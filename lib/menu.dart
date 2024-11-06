4e476a7454b2226509a20ba443c9cd79906b0043
AlyssaLayla <alyssasasti@gmail.com>
2024-11-06 11:37:24 +0700

    Tugas 7 Done

--- lib/menu.dart
+++ lib/menu.dart
@@ -79,23 +79,18 @@ class MyHomePage extends StatelessWidget {
 }
 
 class InfoCard extends StatelessWidget {
-  // Kartu informasi yang menampilkan title dan content.
-
-  final String title;  // Judul kartu.
-  final String content;  // Isi kartu.
+  final String title;  
+  final String content;  
 
   const InfoCard({super.key, required this.title, required this.content});
 
   @override
   Widget build(BuildContext context) {
     return Card(
-      // Membuat kotak kartu dengan bayangan dibawahnya.
       elevation: 2.0,
       child: Container(
-        // Mengatur ukuran dan jarak di dalam kartu.
-        width: MediaQuery.of(context).size.width / 3.5, // menyesuaikan dengan lebar device yang digunakan.
+        width: MediaQuery.of(context).size.width / 3.5, 
         padding: const EdgeInsets.all(16.0),
-        // Menyusun title dan content secara vertikal.
         child: Column(
           children: [
             Text(
@@ -160,6 +155,5 @@ class ItemCard extends StatelessWidget {
       ),
     );
   }
-  
 }
 
